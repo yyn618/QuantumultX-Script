f1db3d0bc31c1fb46d275aca83871ae9a58ba0dd
Xininga <31948921+yyn618@users.noreply.github.com>
2021-10-31 00:55:53 +0800

    Create ali_daily.js

---
+++ Task/ali_daily.js
@@ -0,0 +1,73 @@
+
+#每日蚂蚁收能量提醒（每日壹句（有道词典）+ 点击通知跳转支付宝蚂蚁森林页面
+
+const $tool = new Tool()
+$tool.get('https://dict.youdao.com/infoline/style/cardList?mode=publish&client=mobile&style=daily&size=2', function (error, response, data) {
+    let obj = JSON.parse(data);
+    let date = new Date();
+    let isAM = date.getHours() < 12 ? true : false;
+    let title = '每日' + (isAM ? ' 壹句' : ' 壹句') + (isAM ? ' ☀️' : ' 🌙');
+    let subtitle = '';
+    let scheme = 'alipay://platformapi/startapp?appId=60000002';
+    let content = "该放保护罩了👉"+scheme;
+    let option = {"open-url" : scheme};
+    if (!error) {
+        if (obj && obj.length > 1) {
+            let yi = obj[1];
+            content = yi.title + '\n' + yi.summary + '\n\n' + content;
+            option["media-url"] = yi.image[0];
+        }
+    }
+    $tool.notify(title, subtitle, content, option);
+    $done();
+})
+
+function Tool() {
+    _node = (() => {
+        if (typeof require == "function") {
+            const request = require('request')
+            return ({ request })
+        } else {
+            return (null)
+        }
+    })()
+    _isQuanX = typeof $task != "undefined"
+    this.isQuanX = _isQuanX
+    this.isResponse = typeof $response != "undefined"
+    this.notify = (title, subtitle, message, option) => {
+        if (_isQuanX) $notify(title, subtitle, message, option)
+        if (_node) console.log(JSON.stringify({ title, subtitle, message }));
+    }
+    this.write = (value, key) => {
+        if (_isQuanX) return $prefs.setValueForKey(value, key)
+    }
+    this.read = (key) => {
+        if (_isQuanX) return $prefs.valueForKey(key)
+    }
+    this.get = (options, callback) => {
+        if (_isQuanX) {
+            if (typeof options == "string") options = { url: options }
+            options["method"] = "GET"
+            $task.fetch(options).then(response => { callback(null, _status(response), response.body) }, reason => callback(reason.error, null, null))
+        }
+        if (_node) _node.request(options, (error, response, body) => { callback(error, _status(response), body) })
+    }
+    this.post = (options, callback) => {
+        if (_isQuanX) {
+            if (typeof options == "string") options = { url: options }
+            options["method"] = "POST"
+            $task.fetch(options).then(response => { callback(null, _status(response), response.body) }, reason => callback(reason.error, null, null))
+        }
+        if (_node) _node.request.post(options, (error, response, body) => { callback(error, _status(response), body) })
+    }
+    _status = (response) => {
+        if (response) {
+            if (response.status) {
+                response["statusCode"] = response.status
+            } else if (response.statusCode) {
+                response["status"] = response.statusCode
+            }
+        }
+        return response
+    }
+}
