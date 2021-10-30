bb734998f5d2d6ee8de6235824f7e9c902fdc7fe
Xininga <31948921+yyn618@users.noreply.github.com>
2021-10-31 01:44:31 +0800

    Update dd_daily.js

--- Task/dd_daily.js
+++ Task/dd_daily.js
@@ -1,5 +1,6 @@
-#每日打卡提醒（每日壹句（有道词典）+ 跳转钉钉打卡页面
-
+/*
+每日打卡提醒（每日壹句（有道词典）+ 跳转钉钉打卡页面
+*/
 const $tool = new Tool()
 $tool.get('https://dict.youdao.com/infoline/style/cardList?mode=publish&client=mobile&style=daily&size=2', function (error, response, data) {
     let obj = JSON.parse(data);
