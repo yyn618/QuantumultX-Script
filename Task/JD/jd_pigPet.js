3df2f1982bd1d4615a768e9af0447e722f76ea99
Xininga <31948921+yyn618@users.noreply.github.com>
2021-11-10 23:43:25 +0800

    Update jd_pigPet.js

--- Task/JD/jd_pigPet.js
+++ Task/JD/jd_pigPet.js
@@ -10,10 +10,34 @@ by JDHelloWorld 20211902
 
 活动入口：京东金融-养猪猪
 */
+/*
+Last Modified time: 2021-5-19 12:27:16
+活动入口：京东金融养猪猪
+一键开完所有的宝箱功能。耗时70秒
+大转盘抽奖
+喂食
+每日签到
+完成分享任务得猪粮
+已支持IOS双京东账号,Node.js支持N个京东账号
+脚本兼容: QuantumultX, Surge, Loon, 小火箭，JSBox, Node.js
+===============Quantumultx===============
+[task_local]
+#京东金融养猪猪
+12 0-23/6 * * * jd_pigPet.js, tag=京东金融养猪猪, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jdyz.png, enabled=true
+
+================Loon==============
+[Script]
+cron "12 0-23/6 * * *" script-path=jd_pigPet.js, tag=京东金融养猪猪
+
+===============Surge=================
+京东金融养猪猪 = type=cron,cronexp="12 0-23/6 * * *",wake-system=1,timeout=3600,script-path=jd_pigPet.js
+
+============小火箭=========
+京东金融养猪猪 = type=cron,script-path=jd_pigPet.js, cronexpr="12 0-23/6 * * *", timeout=3600, enable=true
+ */
 
 const $ = new Env('金融养猪');
 const url = require('url');
-const querystring = require('querystring');
 let cookiesArr = [], cookie = '', allMessage = '';
 const JD_API_HOST = 'https://ms.jr.jd.com/gw/generic/uc/h5/m';
 const MISSION_BASE_API = `https://ms.jr.jd.com/gw/generic/mission/h5/m`;
@@ -39,8 +63,16 @@ if ($.isNode()) {
       $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1])
       $.index = i + 1;
       $.isLogin = true;
-      $.nickName = $.UserName;
+      $.nickName = '';
+      await TotalBean();
       console.log(`\n开始【京东账号${$.index}】${$.nickName || $.UserName}\n`);
+      if (!$.isLogin) {
+        $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${$.nickName || $.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/bean/signIndex.action`, {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
+        if ($.isNode()) {
+          await notify.sendNotify(`${$.name}cookie已失效 - ${$.UserName}`, `京东账号${$.index} ${$.UserName}\n请重新登录获取cookie`);
+        }
+        continue
+      }
       await jdPigPet();
     }
   }
@@ -64,9 +96,6 @@ async function jdPigPet() {
     await pigPetOpenBox();
     await pigPetLotteryIndex();
     await pigPetLottery();
-    await pigPetMissionList();
-    await missions();
-    console.log('第一遍完成,开始领取可能的未领取:')
     await pigPetMissionList();
     await missions();
     await pigPetUserBag();
@@ -178,7 +207,7 @@ function pigPetAddFood(skuId) {
   return new Promise(async resolve => {
     console.log(`skuId::::${skuId}`)
     const body = {
-      "source": 0,
+      "source": 2,
       "channelLV":"yqs",
       "riskDeviceParam":"{}",
       "skuId": skuId.toString(),
@@ -418,7 +447,6 @@ function pigPetLotteryPlay() {
 }
 async function missions() {
   for (let item of $.missions) {
-    // console.log(JSON.stringify(item))
     if (item.status === 4) {
       console.log(`\n${item.missionName}任务已做完,开始领取奖励`)
       await pigPetDoMission(item.mid);
@@ -426,28 +454,26 @@ async function missions() {
     } else if (item.status === 5){
       console.log(`\n${item.missionName}已领取`)
     } else if (item.status === 3){
-      // console.log(JSON.stringify(item))
       console.log(`\n${item.missionName}未完成`)
-      await pigPetDoMission(item.mid);
-      await $.wait(1000)
-      let parse
-      if (item.url){
-        parse = url.parse(item.url,true,true)
-        console.log(JSON.stringify(parse))
-      }else{
-        parse = {}
-      }
-      if(parse.query && parse.query.readTime){
-        console.log(`做任务:${item.missionName}`)
-        // console.log(JSON.stringify(item))
-        await queryMissionReceiveAfterStatus(parse.query.missionId)
-        console.log(`等待:${parse.query.readTime}`)
-        await $.wait(parse.query.readTime*1000)
-        await finishReadMission(parse.query.missionId,parse.query.readTime)
+      if (item.mid === 'CPD01') {
+        await pigPetDoMission(item.mid);
+      } else {
+        await pigPetDoMission(item.mid);
         await $.wait(1000)
-      } else if(parse.query && parse.query.juid) {
-        await getJumpInfo(parse.query.juid)
-        await $.wait(4000)
+        let parse
+        if (item.url) {
+          parse = url.parse(item.url, true, true)
+        } else {
+          parse = {}
+        }
+        if (parse.query && parse.query.readTime) {
+          await queryMissionReceiveAfterStatus(parse.query.missionId);
+          await $.wait(parse.query.readTime * 1000)
+          await finishReadMission(parse.query.missionId, parse.query.readTime);
+        } else if (parse.query && parse.query.juid) {
+          await getJumpInfo(parse.query.juid)
+          await $.wait(4000)
+        }
       }
     }
   }
@@ -459,7 +485,7 @@ function pigPetDoMission(mid) {
       "source":2,
       "channelLV":"",
       "riskDeviceParam":"{}",
-      "mid": mid
+      mid
     }
     $.post(taskUrl('pigPetDoMission', body), (err, resp, data) => {
       try {
@@ -533,13 +559,19 @@ function pigPetMissionList() {
 }
 function getJumpInfo(juid) {
   return new Promise(async resolve => {
+    const body = {"juid":juid}
     const options = {
-      "url": `${MISSION_BASE_API}/getJumpInfo?reqData={"juid":"${juid}"}`,
+      "url": `${MISSION_BASE_API}/getJumpInfo?reqData=${escape(JSON.stringify(body))}`,
       "headers": {
+        'Host': 'ms.jr.jd.com',
+        'Origin': 'https://active.jd.com',
+        'Connection': 'keep-alive',
+        'Accept': 'application/json',
         "Cookie": cookie,
-        'content-type': 'application/json',
-        'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148/application=JDJR-App&deviceId=1423833363730383d273532393d243445364-d224341443d2938333530323445433033353&eufv=1&clientType=ios&iosType=iphone&clientVersion=6.1.70&HiClVersion=6.1.70&isUpdate=0&osVersion=13.7&osName=iOS&platform=iPhone 6s (A1633/A1688/A1691/A1700)&screen=667*375&src=App Store&netWork=1&netWorkType=1&CpayJS=UnionPay/1.0 JDJR&stockSDK=stocksdk-iphone_3.5.0&sPoint=&jdPay=(*#@jdPaySDK*#@jdPayChannel=jdfinance&jdPayChannelVersion=6.1.70&jdPaySdkVersion=3.00.52.00&jdPayClientName=iOS*#@jdPaySDK*#@)',
-        'referer': 'https://u1.jr.jd.com/uc-fe-wxgrowing/cloudpig/index/?channel=gry&jrcontainer=h5&jrlogin=true',
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148/application=JDJR-App&deviceId=1423833363730383d273532393d243445364-d224341443d2938333530323445433033353&eufv=1&clientType=ios&iosType=iphone&clientVersion=6.1.70&HiClVersion=6.1.70&isUpdate=0&osVersion=13.7&osName=iOS&platform=iPhone 6s (A1633/A1688/A1691/A1700)&screen=667*375&src=App Store&netWork=1&netWorkType=1&CpayJS=UnionPay/1.0 JDJR&stockSDK=stocksdk-iphone_3.5.0&sPoint=&jdPay=(*#@jdPaySDK*#@jdPayChannel=jdfinance&jdPayChannelVersion=6.1.70&jdPaySdkVersion=3.00.52.00&jdPayClientName=iOS*#@jdPaySDK*#@)',
+        'Accept-Language': 'zh-cn',
+        'Referer': 'https://u1.jr.jd.com/uc-fe-wxgrowing/cloudpig/index/',
+        'Accept-Encoding': 'gzip, deflate, br'
       }
     }
     $.get(options, (err, resp, data) => {
@@ -564,13 +596,19 @@ function getJumpInfo(juid) {
 }
 function queryMissionReceiveAfterStatus(missionId) {
   return new Promise(resolve => {
+    const body = {"missionId": missionId};
     const options = {
-      "url": `${MISSION_BASE_API}/queryMissionReceiveAfterStatus?reqData={"missionId":"${missionId}"}`,
+      "url": `${MISSION_BASE_API}/queryMissionReceiveAfterStatus?reqData=${escape(JSON.stringify(body))}`,
       "headers": {
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.9",
+        "Connection": "keep-alive",
+        "Host": "ms.jr.jd.com",
         "Cookie": cookie,
-        'content-type': 'application/json',
-        'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148/application=JDJR-App&deviceId=1423833363730383d273532393d243445364-d224341443d2938333530323445433033353&eufv=1&clientType=ios&iosType=iphone&clientVersion=6.1.70&HiClVersion=6.1.70&isUpdate=0&osVersion=13.7&osName=iOS&platform=iPhone 6s (A1633/A1688/A1691/A1700)&screen=667*375&src=App Store&netWork=1&netWorkType=1&CpayJS=UnionPay/1.0 JDJR&stockSDK=stocksdk-iphone_3.5.0&sPoint=&jdPay=(*#@jdPaySDK*#@jdPayChannel=jdfinance&jdPayChannelVersion=6.1.70&jdPaySdkVersion=3.00.52.00&jdPayClientName=iOS*#@jdPaySDK*#@)',
-        'referer': 'https://u1.jr.jd.com/uc-fe-wxgrowing/cloudpig/index/?channel=gry&jrcontainer=h5&jrlogin=true',
+        "Origin": "https://jdjoy.jd.com",
+        "Referer": "https://jdjoy.jd.com/",
+        "User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1")
       }
     }
     $.get(options, (err, resp, data) => {
@@ -594,15 +632,21 @@ function queryMissionReceiveAfterStatus(missionId) {
   })
 }
 //做完浏览任务发送信息API
-function finishReadMission(missionId,readTime) {
+function finishReadMission(missionId, readTime) {
   return new Promise(async resolve => {
+    const body = {"missionId":missionId,"readTime":readTime * 1};
     const options = {
-      "url": `${MISSION_BASE_API}/finishReadMission?reqData={"missionId":"${missionId}","readTime":${readTime}}`,
+      "url": `${MISSION_BASE_API}/finishReadMission?reqData=${escape(JSON.stringify(body))}`,
       "headers": {
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.9",
+        "Connection": "keep-alive",
+        "Host": "ms.jr.jd.com",
         "Cookie": cookie,
-        'content-type': 'application/json',
-        'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148/application=JDJR-App&deviceId=1423833363730383d273532393d243445364-d224341443d2938333530323445433033353&eufv=1&clientType=ios&iosType=iphone&clientVersion=6.1.70&HiClVersion=6.1.70&isUpdate=0&osVersion=13.7&osName=iOS&platform=iPhone 6s (A1633/A1688/A1691/A1700)&screen=667*375&src=App Store&netWork=1&netWorkType=1&CpayJS=UnionPay/1.0 JDJR&stockSDK=stocksdk-iphone_3.5.0&sPoint=&jdPay=(*#@jdPaySDK*#@jdPayChannel=jdfinance&jdPayChannelVersion=6.1.70&jdPaySdkVersion=3.00.52.00&jdPayClientName=iOS*#@jdPaySDK*#@)',
-        'referer': 'https://u1.jr.jd.com/uc-fe-wxgrowing/cloudpig/index/?channel=gry&jrcontainer=h5&jrlogin=true',
+        "Origin": "https://jdjoy.jd.com",
+        "Referer": "https://jdjoy.jd.com/",
+        "User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1")
       }
     }
     $.get(options, (err, resp, data) => {
@@ -625,15 +669,65 @@ function finishReadMission(missionId,readTime) {
     })
   })
 }
+function TotalBean() {
+  return new Promise(async resolve => {
+    const options = {
+      "url": `https://wq.jd.com/user/info/QueryJDUserInfo?sceneval=2`,
+      "headers": {
+        "Accept": "application/json,text/plain, */*",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-cn",
+        "Connection": "keep-alive",
+        "Cookie": cookie,
+        "Referer": "https://wqs.jd.com/my/jingdou/my.shtml?sceneval=2",
+        "User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1")
+      }
+    }
+    $.post(options, (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(`${JSON.stringify(err)}`)
+          console.log(`${$.name} API请求失败，请检查网路重试`)
+        } else {
+          if (data) {
+            data = JSON.parse(data);
+            if (data['retcode'] === 13) {
+              $.isLogin = false; //cookie过期
+              return
+            }
+            if (data['retcode'] === 0) {
+              $.nickName = (data['base'] && data['base'].nickname) || $.UserName;
+            } else {
+              $.nickName = $.UserName
+            }
+          } else {
+            console.log(`京东服务器返回空数据`)
+          }
+        }
+      } catch (e) {
+        $.logErr(e, resp)
+      } finally {
+        resolve();
+      }
+    })
+  })
+}
 function taskUrl(function_id, body) {
   return {
     url: `${JD_API_HOST}/${function_id}?_=${Date.now()}`,
-    body: `reqData=${JSON.stringify(body)}`,
-    "headers": {
-      "Cookie": cookie,
-      'content-type': 'application/json',
-      'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148/application=JDJR-App&deviceId=1423833363730383d273532393d243445364-d224341443d2938333530323445433033353&eufv=1&clientType=ios&iosType=iphone&clientVersion=6.1.70&HiClVersion=6.1.70&isUpdate=0&osVersion=13.7&osName=iOS&platform=iPhone 6s (A1633/A1688/A1691/A1700)&screen=667*375&src=App Store&netWork=1&netWorkType=1&CpayJS=UnionPay/1.0 JDJR&stockSDK=stocksdk-iphone_3.5.0&sPoint=&jdPay=(*#@jdPaySDK*#@jdPayChannel=jdfinance&jdPayChannelVersion=6.1.70&jdPaySdkVersion=3.00.52.00&jdPayClientName=iOS*#@jdPaySDK*#@)',
-      'referer': 'https://u1.jr.jd.com/uc-fe-wxgrowing/cloudpig/index/?channel=gry&jrcontainer=h5&jrlogin=true',
+    body: `reqData=${encodeURIComponent(JSON.stringify(body))}`,
+    headers: {
+      'Accept' : `*/*`,
+      'Origin' : `https://u.jr.jd.com`,
+      'Accept-Encoding' : `gzip, deflate, br`,
+      'Cookie' : cookie,
+      'Content-Type' : `application/x-www-form-urlencoded;charset=UTF-8`,
+      'Host' : `ms.jr.jd.com`,
+      'Connection' : `keep-alive`,
+      'User-Agent' : `jdapp;android;8.5.12;9;network/wifi;model/GM1910;addressid/1302541636;aid/ac31e03386ddbec6;oaid/;osVer/28;appBuild/73078;adk/;ads/;pap/JA2015_311210|8.5.12|ANDROID 9;osv/9;pv/117.24;jdv/0|kong|t_1000217905_|jingfen|644e9b005c8542c1ac273da7763971d8|1589905791552|1589905794;ref/com.jingdong.app.mall.WebActivity;partner/oppo;apprpd/Home_Main;Mozilla/5.0 (Linux; Android 9; GM1910 Build/PKQ1.190110.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 MQQBrowser/6.2 TBS/044942 Mobile Safari/537.36 Edg/86.0.4240.111`,
+      'Referer' : `https://u.jr.jd.com/`,
+      'Accept-Language' : `zh-cn`
     }
   }
 }
