2cea8625031cd41c2c87015392530e9858d98780
Xininga <31948921+yyn618@users.noreply.github.com>
2021-11-10 22:56:23 +0800

    Update jd_cash.js

--- Task/JD/jd_cash.js
+++ Task/JD/jd_cash.js
@@ -1,30 +1,9 @@
 /*
-by JDHelloWorld 20211105
-2 0-23/4 * * * https://raw.githubusercontent.com/yyn618/QuantumultX-Script/master/Script/Files/JD/jd_cash.js, tag=签到领现金, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
-
+by chuhe 20210607
+2 0-23/4 * * * https://raw.githubusercontent.com/yyn618/QuantumultX-Script/master/Task/JD/jd_cash.js, tag=签到领现金, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
 活动入口：京东APP搜索领现金进入
-*//*
-签到领现金，每日2毛～5毛
-可互助，助力码每日不变，只变日期
-活动入口：京东APP搜索领现金进入
-更新时间：2021-06-07
-已支持IOS双京东账号,Node.js支持N个京东账号
-脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
-============Quantumultx===============
-[task_local]
-#签到领现金
-2 0-23/4 * * * jd_cash.js, tag=签到领现金, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
+*/
 
-================Loon==============
-[Script]
-cron "2 0-23/4 * * *" script-path=jd_cash.js,tag=签到领现金
-
-===============Surge=================
-签到领现金 = type=cron,cronexp="2 0-23/4 * * *",wake-system=1,timeout=3600,script-path=jd_cash.js
-
-============小火箭=========
-签到领现金 = type=cron,script-path=jd_cash.js, cronexpr="2 0-23/4 * * *", timeout=3600, enable=true
- */
 const $ = new Env('签到领现金');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
