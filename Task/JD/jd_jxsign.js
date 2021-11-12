7ae455fc5aad04045f6e0163cb2470b6ecbcbda1
Xininga <31948921+yyn618@users.noreply.github.com>
2021-11-12 15:37:13 +0800

    Update jd_jxsign.js

--- Task/JD/jd_jxsign.js
+++ Task/JD/jd_jxsign.js
@@ -1,6 +1,6 @@
 /*
 by lxk0301 20211110
-20 1,8 * * * https://raw.githubusercontent.com/yyn618/QuantumultX-Script/master/Script/Files/JD/jd_jxsign.js, tag=京喜签到, mg-url=https://raw.githubusercontent.com/58xinian/icon/master/jxcfd.png, enabled=true
+20 1,8 * * * https://raw.githubusercontent.com/yyn618/QuantumultX-Script/master/Script/Files/JD/jd_jxsign.js, tag=京喜签到, mg-url=https://raw.githubusercontent.com/58xinian/icon/master/jd.png, enabled=true
 
 活动入口：京喜APP-我的-京喜签到
 */
